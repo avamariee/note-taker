a4e77f9d77bfbc81fde3491efd234f31036ae1b8
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-07 19:43:21 -0600

    successfully displayed test JSON

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+
+
+const notes = require('./db/db.json')
+
+
+app.get('/db/db', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on port ${PORT}!`);
 })
