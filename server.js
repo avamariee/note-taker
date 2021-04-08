41c9709dfbc1204def03a71f360ac7647661e576
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-07 19:49:59 -0600

    added GET routes for index.html and notes.html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,6 +9,15 @@ const app = express();
 
 const notes = require('./db/db.json')
 
+// get path to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+// get path to notes.html after clicking "get started" button on index.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 
 app.get('/db/db', (req, res) => {
     let results = notes;
