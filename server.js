77c181b36d976e36beadb09fef38d5c41952c03a
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-07 20:30:09 -0600

    Successfully added new data to the JSON file, tested with Insomnia

--- server.js
+++ server.js
@@ -4,18 +4,33 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 const notes = require('./db/db.json')
 
+
+// function to create new note data
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    // add the data to the db.json file
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    )
+
+    return note;
+}
 
 
 // get path to notes.html after clicking "get started" button on index.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
-
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
@@ -43,13 +58,17 @@ app.get('*', (req, res) => {
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body
 
-    console.log(newNote);
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
-    notes.push(newNote);
+    const newNote = createNewNote(req.body, notes);
 
-    res.json(newNote);
+    // console.log(newNote);
+
+    // notes.push(newNote);
+
+    res.json(notes);
 })
 
 
